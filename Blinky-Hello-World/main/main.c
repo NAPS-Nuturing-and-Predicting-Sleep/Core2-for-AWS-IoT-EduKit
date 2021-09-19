f06924db8c4284b2f91b6b817ce095d81702c4d7
aquaMatt900 <71993233+aquaMatt900@users.noreply.github.com>
2021-09-19 14:36:22 -0400

    Updated to include sensorData() function and packaging/publishing methods for the sleep monitoring system

--- Blinky-Hello-World/main/main.c
+++ Blinky-Hello-World/main/main.c
@@ -1,6 +1,6 @@
 /*
  * AWS IoT EduKit - Core2 for AWS IoT EduKit
- * Cloud Connected Blinky v1.4.0
+ * Cloud Connected Blinky v1.3.0
  * main.c
  * 
  * Copyright 2010-2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
@@ -39,6 +39,7 @@
 #include "freertos/semphr.h"
 #include "freertos/event_groups.h"
 #include "esp_log.h"
+#include "cJSON.h"
 
 #include "aws_iot_config.h"
 #include "aws_iot_log.h"
@@ -51,8 +52,20 @@
 #include "blink.h"
 #include "ui.h"
 
+#define STARTING_ROOMTEMPERATURE 0.0f
+
 /* The time between each MQTT message publish in milliseconds */
-#define PUBLISH_INTERVAL_MS 3000
+#define PUBLISH_INTERVAL_MS 5000
+
+/* The time between each sensor data gathering period (in seconds)*/
+#define SENSOR_READING_AVERAGE_DELAY 4.6
+
+/* The time between reading the sensor data once, before the next read (in seconds). 
+   This should be a factor of SENSOR_READING_AVERAGE_DELAY, otherwise you risk doing sensor readings beyond 5 seconds*/
+#define SENSOR_READING_DELAY 0.2
+
+/* Specify variable for keeping track of time in vTaskDelayUntil */
+TickType_t xLastWakeTime;
 
 /* The time prefix used by the logger. */
 static const char *TAG = "MAIN";
@@ -108,33 +121,117 @@ void disconnect_callback_handler(AWS_IoT_Client *pClient, void *data) {
     }
 }
 
-static void publisher(AWS_IoT_Client *client, char *base_topic, uint16_t base_topic_len){
-    char cPayload[100];
-    int32_t i = 0;
+/* Initialize all the variables to be used for gathering sensor data and averages */
+float temperature = 0;
+float gx = 0;
+float gy = 0;
+float gz = 0;
+float ax = 0;
+float ay = 0;
+float az = 0;
 
-    IoT_Publish_Message_Params paramsQOS0;
-    IoT_Publish_Message_Params paramsQOS1;
+float avg_temperature = 0;
+float avg_gx = 0;
+float avg_gy = 0;
+float avg_gz = 0;
+float avg_ax = 0;
+float avg_ay = 0;
+float avg_az = 0;
 
-    paramsQOS0.qos = QOS0;
-    paramsQOS0.payload = (void *) cPayload;
-    paramsQOS0.isRetained = 0;
+float Sum_Temperature = 0;
+float Sum_Gyro_x = 0;
+float Sum_Gyro_y = 0;
+float Sum_Gyro_z = 0;
+float Sum_Accel_x = 0;
+float Sum_Accel_y = 0;
+float Sum_Accel_z = 0;
 
-    // Publish and ignore if "ack" was received or  from AWS IoT Core
-    sprintf(cPayload, "%s : %d ", "Hello from AWS IoT EduKit (QOS0)", i++);
-    paramsQOS0.payloadLen = strlen(cPayload);
-    IoT_Error_t rc = aws_iot_mqtt_publish(client, base_topic, base_topic_len, &paramsQOS0);
-    if (rc != SUCCESS){
-        ESP_LOGE(TAG, "Publish QOS0 error %i", rc);
-        rc = SUCCESS;
+/* This function will pull sensor data every ~0.05s and sum it. After ~4.9s, the data gets averaged, which is what can be pulled on the output */
+void sensorData(float *Avg_Gyro_x, float *Avg_Gyro_y, float *Avg_Gyro_z, float *Avg_Accel_x, float *Avg_Accel_y, float *Avg_Accel_z, float *Avg_Temperature){
+
+/* Reset all summing variables and the counter to zero */
+    Sum_Temperature = 0;
+    Sum_Gyro_x = 0;
+    Sum_Gyro_y = 0;
+    Sum_Gyro_z = 0;
+    Sum_Accel_x = 0;
+    Sum_Accel_y = 0;
+    Sum_Accel_z = 0;
+    int count_i = 0;
+
+/* Create a basic timer to control the data collection loop, reporting averages according to SENSOR_READING_AVERAGE_DELAY */
+    clock_t begin_Time = clock();
+
+    while ( ((unsigned long) (clock() - begin_Time)/CLOCKS_PER_SEC) < SENSOR_READING_AVERAGE_DELAY){
+
+        //printf("Start Sensor Gathering = %lf\n", (double) clock()/CLOCKS_PER_SEC);  ///////////////  debugging code
+        MPU6886_GetTempData(&temperature);
+        Sum_Temperature += (temperature * 1.8)  + 32 - 80;
+
+/*Taking the square of each value so that averaging makes more sense with both positives and negatives coming in, 
+  and so motion is weighted more (trying to pick up smaller motion)*/
+        MPU6886_GetAccelData(&ax, &ay, &az);
+        Sum_Accel_x += ax*ax;  
+        Sum_Accel_y += ay*ay;
+        Sum_Accel_z += az*az;
+
+        MPU6886_GetGyroData(&gx, &gy, &gz);
+        Sum_Gyro_x += gx*gx;
+        Sum_Gyro_y += gy*gy;
+        Sum_Gyro_z += gz*gz;
+
+        //printf("End Sensor Gathering = %lf\n", (double) clock()/CLOCKS_PER_SEC);  //////////////  debugging code
+
+        count_i++;
+
+/* To keep from accumulating huge numbers in the Sum_xxx variables, the while loop below delays for SENSOR_READING_DELAY
+   This is probably not the best implementation, but should work for the sleep monitor's purposes*/
+        clock_t start_Time = clock();
+        while ( ((unsigned long) (clock() - start_Time)/CLOCKS_PER_SEC) < SENSOR_READING_DELAY){}
     }
 
+/* Calculate average values over the SENSOR_READING_AVERAGE_DELAY period */
+    *Avg_Temperature = Sum_Temperature/count_i;
+    *Avg_Accel_x = Sum_Accel_x/count_i;
+    *Avg_Accel_y = Sum_Accel_y/count_i;
+    *Avg_Accel_z = Sum_Accel_z/count_i;
+    *Avg_Gyro_x = Sum_Gyro_x/count_i;
+    *Avg_Gyro_y = Sum_Gyro_y/count_i;
+    *Avg_Gyro_z = Sum_Gyro_z/count_i;
+
+}
+
+static void publisher(AWS_IoT_Client *client, char *base_topic, uint16_t base_topic_len){
+
+    IoT_Publish_Message_Params paramsQOS1;
     paramsQOS1.qos = QOS1;
-    paramsQOS1.payload = (void *) cPayload;
     paramsQOS1.isRetained = 0;
+
+    cJSON *payload = cJSON_CreateObject();
+
+    cJSON *temperature_value = cJSON_CreateNumber(avg_temperature);
+    cJSON_AddItemToObject(payload, "temperature_value", temperature_value);
+
+    cJSON *accel_x_value = cJSON_CreateNumber(avg_ax);
+    cJSON *accel_y_value = cJSON_CreateNumber(avg_ay);
+    cJSON *accel_z_value = cJSON_CreateNumber(avg_az);
+    cJSON_AddItemToObject(payload, "accel_x_value", accel_x_value);
+    cJSON_AddItemToObject(payload, "accel_y_value", accel_y_value);
+    cJSON_AddItemToObject(payload, "accel_z_value", accel_z_value);
+
+    cJSON *gyro_x_value = cJSON_CreateNumber(avg_gx);
+    cJSON *gyro_y_value = cJSON_CreateNumber(avg_gy);
+    cJSON *gyro_z_value = cJSON_CreateNumber(avg_gz);
+    cJSON_AddItemToObject(payload, "gyro_x_value", gyro_x_value);
+    cJSON_AddItemToObject(payload, "gyro_y_value", gyro_y_value);
+    cJSON_AddItemToObject(payload, "gyro_z_value", gyro_z_value);
+
+    const char *JSONPayload = cJSON_Print(payload);
+    paramsQOS1.payload = (void*) JSONPayload;
+    paramsQOS1.payloadLen = strlen(JSONPayload);
+
     // Publish and check if "ack" was sent from AWS IoT Core
-    sprintf(cPayload, "%s : %d ", "Hello from AWS IoT EduKit (QOS1)", i++);
-    paramsQOS1.payloadLen = strlen(cPayload);
-    rc = aws_iot_mqtt_publish(client, base_topic, base_topic_len, &paramsQOS1);
+    IoT_Error_t rc = aws_iot_mqtt_publish(client, base_topic, base_topic_len, &paramsQOS1);
     if (rc == MQTT_REQUEST_TIMEOUT_ERROR) {
         ESP_LOGW(TAG, "QOS1 publish ack not received.");
         rc = SUCCESS;
@@ -142,6 +239,7 @@ static void publisher(AWS_IoT_Client *client, char *base_topic, uint16_t base_to
 }
 
 void aws_iot_task(void *param) {
+    //printf("Start iot Task = %lf\n", (double) clock()/CLOCKS_PER_SEC);  //////////////  debugging code
     IoT_Error_t rc = FAILURE;
 
     AWS_IoT_Client client;
@@ -208,7 +306,6 @@ void aws_iot_task(void *param) {
     } while(SUCCESS != rc);
     ui_textarea_add("Successfully connected!\n", NULL, 0);
     ESP_LOGI(TAG, "Successfully connected to AWS IoT Core!");
-
     /*
      * Enable Auto Reconnect functionality. Minimum and Maximum time for exponential backoff for retries.
      *  #AWS_IOT_MQTT_MIN_RECONNECT_WAIT_INTERVAL
@@ -237,7 +334,7 @@ void aws_iot_task(void *param) {
     
     ui_textarea_add("Attempting publish to: %s\n", base_publish_topic, BASE_PUBLISH_TOPIC_LEN) ;
     while((NETWORK_ATTEMPTING_RECONNECT == rc || NETWORK_RECONNECTED == rc || SUCCESS == rc)) {
-
+        
         //Max time the yield function will wait for read messages
         rc = aws_iot_mqtt_yield(&client, 100);
         if(NETWORK_ATTEMPTING_RECONNECT == rc) {
@@ -246,9 +343,12 @@ void aws_iot_task(void *param) {
         }
 
         ESP_LOGD(TAG, "Stack remaining for task '%s' is %d bytes", pcTaskGetTaskName(NULL), uxTaskGetStackHighWaterMark(NULL));
-        vTaskDelay(pdMS_TO_TICKS(PUBLISH_INTERVAL_MS));
-        
+
+        sensorData(&avg_gx, &avg_gy, &avg_gz, &avg_ax, &avg_ay, &avg_az, &avg_temperature);
         publisher(&client, base_publish_topic, BASE_PUBLISH_TOPIC_LEN);
+        //printf("End iot Task Loop = %lf\n", (double) clock()/CLOCKS_PER_SEC);  //////////////  debugging code
+        vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(PUBLISH_INTERVAL_MS));
+        //printf("Start iot Task Loop = %lf\n", (double) clock()/CLOCKS_PER_SEC);  //////////////  debugging code
     }
 
     ESP_LOGE(TAG, "An error occurred in the main loop.");
@@ -258,10 +358,13 @@ void aws_iot_task(void *param) {
 void app_main()
 {
     Core2ForAWS_Init();
-    Core2ForAWS_Display_SetBrightness(80);
+    Core2ForAWS_Display_SetBrightness(25);
     
     ui_init();
     initialise_wifi();
+
+    /* Initialize the tick time for use in vTaskDelayUntil*/
+    xLastWakeTime = xTaskGetTickCount();
 
     xTaskCreatePinnedToCore(&aws_iot_task, "aws_iot_task", 4096 * 2, NULL, 5, NULL, 1);
     xTaskCreatePinnedToCore(&blink_task, "blink_task", 4096 * 1, NULL, 2, &xBlink, 1);
